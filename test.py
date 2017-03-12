a29792ab7a0f841d3c8a5ee0fe68e172ed2e3845
chriscohoat <chris.cohoat@gmail.com>
2017-03-11 20:01:49 -0500

    Added simple test

---
+++ test.py
@@ -0,0 +1,27 @@
+import time
+
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
+
+from PIL import Image
+
+# Raspberry Pi pin configuration:
+RST = 26
+DC = 19
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
+
+# Initialize library.
+disp.begin()
+
+# Clear display.
+disp.clear()
+disp.display()
+
+image = Image.open('test.png').resize((disp.width, disp.height), Image.ANTIALIAS).convert('1')
+
+# Display image.
+disp.image(image)
+disp.display()
